d426fea0e677654e7df95792943f95e8618b1316
Halva773 <selin0106@mail.ru>
2024-07-24 22:00:01 +0300

    Написал функцию подсчёта сыграных и выиграных раундов за каждую сторону. Как мне кажется, код излишне большой (так получилось из-за допольнительных раундов)

--- demoParser.py
+++ demoParser.py
@@ -12,11 +12,15 @@ class GameStat:
         self.player_death_info = self.parser.parse_event("player_death",
                                                          player=player_stats,
                                                          other=["total_rounds_played", "is_warmup_period"])
-        self.__cnt_rounds = self.cnt_rounds()
+        self.round_ended = self.parser.parse_event("round_end", other=["total_rounds_played"])
+        self.round_ended = self.round_ended[self.round_ended.winner.isna() == False].drop_duplicates(
+            subset='total_rounds_played', keep='last').reset_index()
+        self.__cnt_rounds = len(self.round_ended)
         # self.player_death_info = self.parser.parse_event("player_death", other=["total_rounds_played"])
 
     def complain_data(self):
-        self.result_df = pd.concat([self.base_stat(), self.total_damage(), self.utility_damage(), self.sniper_kilss()], axis=1)
+        self.result_df = pd.concat([self.base_stat(), self.total_damage(), self.utility_damage(), self.sniper_kilss()],
+                                   axis=1)
         # self.result_df.columns = ['total_assits', 'total_deaths', 'total_kills', 'total_damage', 'utility_damage', '3_kills', '4_kills', '5_kills', 'AWP_kills']
 
         self.result_df['rounds_played'] = self.__cnt_rounds
@@ -59,11 +63,6 @@ class GameStat:
         columns = ['kills_total', 'deaths_total', 'headshot_kills_total', 'ace_rounds_total', '4k_rounds_total',
                    '3k_rounds_total']
         return df[columns]
-
-
-
-    def cnt_rounds(self):
-        return self.player_death_info.total_rounds_played.max()
 
     def find_if_yVx(self, round_idx, round_ends, df, X, y):
         for _, death in self.player_death_info.iterrows():
@@ -170,15 +169,130 @@ class GameStat:
         return player_death_info[(player_death_info['user_team_name'] == 'CT') & (
                 player_death_info['total_rounds_played'] == 0)].groupby('user_name').max()['user_team_name']
 
-
     def sniper_kilss(self):
         columns_list = ['attacker_name', 'weapon']
-        return self.player_death_info[self.player_death_info['weapon'] == 'awp'][columns_list].groupby('attacker_name').count().rename(columns={'weapon': 'AWP_kills'})
+        return self.player_death_info[self.player_death_info['weapon'] == 'awp'][columns_list].groupby(
+            'attacker_name').count().rename(columns={'weapon': 'AWP_kills'})
+
+    def sides_won(self):
+        t_side = {'ct started team': [0, 0],
+                  't started team': [0, 0]}
+        ct_side = {'ct started team': [0, 0],
+                   't started team': [0, 0]}
+        first_half = self.round_ended.iloc[:12, :].groupby('winner').count()['tick']
+        if len(first_half) == 2:
+            t_side['t started team'] = [12, int(first_half['T'])]
+            ct_side['ct started team'] = [12, int(first_half['CT'])]
+        else:
+            if first_half.index[0] == 'T':
+                t_side['t started team'] = [12, 12]
+                ct_side['ct started team'] = [12, 0]
+            else:
+                t_side['t started team'] = [12, 0]
+                ct_side['ct started team'] = [12, 12]
+        second_half = self.round_ended.iloc[12:24, :].groupby('winner').count()['tick']
+        second_half_rounds_played = len(self.round_ended.iloc[12:24, :])
+        if len(second_half) == 2:
+            t_side['ct started team'] = [second_half_rounds_played, int(second_half['T'])]
+            ct_side['t started team'] = [second_half_rounds_played, int(second_half['CT'])]
+        else:
+            if second_half.index[0] == 'T':
+                t_side['ct started team'] = [second_half_rounds_played, second_half_rounds_played]
+                ct_side['t started team'] = [second_half_rounds_played, 0]
+            else:
+                t_side['ct started team'] = [second_half_rounds_played, 0]
+                ct_side['t started team'] = [second_half_rounds_played, second_half_rounds_played]
+        extra_times = 0
+        while self.__cnt_rounds > 24 + extra_times * 6:
+            extra_times += 1
+            if extra_times % 2 != 0:
+                first_half = \
+                self.round_ended.iloc[24 + (extra_times - 1) * 6:24 + (extra_times - 1) * 6 + 3, :].groupby(
+                    'winner').count()['tick']
+                first_half_rounds_played = 3
+                if len(first_half) == 2:
+                    t_side['ct started team'] = [t_side['ct started team'][0] + first_half_rounds_played,
+                                                 t_side['ct started team'][1] + int(first_half['T'])]
+                    ct_side['t started team'] = [ct_side['t started team'][0] + first_half_rounds_played,
+                                                 ct_side['t started team'][1] + int(first_half['CT'])]
+                else:
+                    if first_half.index[0] == 'T':
+                        t_side['ct started team'] = [t_side['ct started team'][0] + first_half_rounds_played,
+                                                     t_side['ct started team'][1] + first_half_rounds_played]
+                        ct_side['t started team'] = [ct_side['t started team'][0] + first_half_rounds_played,
+                                                     ct_side['t started team'][1]]
+                    else:
+                        t_side['ct started team'] = [t_side['ct started team'][0] + first_half_rounds_played,
+                                                     t_side['ct started team'][1]]
+                        ct_side['t started team'] = [ct_side['t started team'][0] + first_half_rounds_played,
+                                                     ct_side['t started team'][1] + first_half_rounds_played]
+
+                second_half = self.round_ended.iloc[24 + (extra_times - 1) * 6 + 3:24 + (extra_times - 1) * 6 + 6, :]
+                second_half_rounds_played = len(second_half)
+                second_half = second_half.groupby('winner').count()['tick']
+                if len(second_half) == 2:
+                    t_side['t started team'] = [t_side['t started team'][0] + second_half_rounds_played,
+                                                t_side['t started team'][1] + int(second_half['T'])]
+                    ct_side['ct started team'] = [ct_side['ct started team'][0] + second_half_rounds_played,
+                                                  ct_side['ct started team'][1] + int(second_half['CT'])]
+                else:
+                    if second_half.index[0] == 'T':
+                        t_side['t started team'] = [t_side['t started team'][0] + second_half_rounds_played,
+                                                    t_side['t started team'][1] + second_half_rounds_played]
+                        ct_side['ct started team'] = [ct_side['ct started team'][0] + second_half_rounds_played,
+                                                      ct_side['ct started team'][1]]
+                    else:
+                        t_side['t started team'] = [t_side['t started team'][0] + second_half_rounds_played,
+                                                    t_side['t started team'][1]]
+                        ct_side['ct started team'] = [ct_side['ct started team'][0] + second_half_rounds_played,
+                                                      ct_side['ct started team'][1] + second_half_rounds_played]
+            else:
+                first_half = self.round_ended.iloc[24 + (extra_times - 1) * 6:24 + (extra_times - 1) * 6 + 3, :] \
+                                                                                .groupby('winner').count()['tick']
+                first_half_rounds_played = 3
+                if len(first_half) == 2:
+                    t_side['t started team'] = [t_side['t started team'][0] + first_half_rounds_played,
+                                                 t_side['t started team'][1] + int(first_half['T'])]
+                    ct_side['ct started team'] = [ct_side['ct started team'][0] + first_half_rounds_played,
+                                                 ct_side['ct started team'][1] + int(first_half['CT'])]
+                else:
+                    if first_half.index[0] == 'T':
+                        t_side['t started team'] = [t_side['t started team'][0] + first_half_rounds_played,
+                                                     t_side['t started team'][1] + first_half_rounds_played]
+                        ct_side['ct started team'] = [ct_side['ct started team'][0] + first_half_rounds_played,
+                                                     ct_side['ct started team'][1]]
+                    else:
+                        t_side['t started team'] = [t_side['t started team'][0] + first_half_rounds_played,
+                                                     t_side['t started team'][1]]
+                        ct_side['ct started team'] = [ct_side['ct started team'][0] + first_half_rounds_played,
+                                                     ct_side['ct started team'][1] + first_half_rounds_played]
+
+                second_half = self.round_ended.iloc[24 + (extra_times - 1) * 6 + 3:24 + (extra_times - 1) * 6 + 6, :]
+                second_half_rounds_played = len(second_half)
+                second_half = second_half.groupby('winner').count()['tick']
+                if len(second_half) == 2:
+                    t_side['ct started team'] = [t_side['ct started team'][0] + second_half_rounds_played,
+                                                t_side['ct started team'][1] + int(second_half['T'])]
+                    ct_side['t started team'] = [ct_side['t started team'][0] + second_half_rounds_played,
+                                                  ct_side['t started team'][1] + int(second_half['CT'])]
+                else:
+                    if second_half.index[0] == 'T':
+                        t_side['ct started team'] = [t_side['ct started team'][0] + second_half_rounds_played,
+                                                    t_side['ct started team'][1] + second_half_rounds_played]
+                        ct_side['t started team'] = [ct_side['t started team'][0] + second_half_rounds_played,
+                                                      ct_side['t started team'][1]]
+                    else:
+                        t_side['ct started team'] = [t_side['ct started team'][0] + second_half_rounds_played,
+                                                    t_side['ct started team'][1]]
+                        ct_side['t started team'] = [ct_side['t started team'][0] + second_half_rounds_played,
+                                                      ct_side['t started team'][1] + second_half_rounds_played]
+        return t_side, ct_side
 
 
 def main():
     # https://www.hltv.org/matches/2373289/g2-vs-natus-vincere-esports-world-cup-2024
-    src = "demos/mouz-nxt-vs-rhyno-m1-ancient.dem"
+    src = 'demos/g2-vs-natus-vincere-m1-ancient.dem'
+    # src = "demos/mouz-nxt-vs-rhyno-m1-ancient.dem"
     stat = GameStat(src)
     df = stat.complain_data()
     print(df.columns)
@@ -188,6 +302,7 @@ def main():
 
 
 if __name__ == '__main__':
-    stat = main()
-    columns = ['start_side', '5v4_situations', '5v4_wins', '4v5_situations', '4v5_wins']
-    print(stat.result_df[columns])
+    # stat = main()
+    src = 'demos/red-canids-vs-kru-nuke.dem'
+    stat = GameStat(src)
+    print(stat.sides_won())
